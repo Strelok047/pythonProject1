44eeddeac87efba67172b86ab1c924dc02e98133
Bekzhan Bekenov <bekzhanbekenov2003@gmail.com>
2025-04-08 20:46:32 +0500

    new

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ import zipfile
 import tempfile
 import os
 
+# Получаем токен из секретов Streamlit
+earthengine_token = os.getenv("EARTHENGINE_TOKEN")
+
+# Инициализация Earth Engine с токеном
+ee.Initialize(credentials=earthengine_token)
 
 def setup():
     st.set_page_config(layout="wide", page_title="Satellite imagery", page_icon='🛰️')
